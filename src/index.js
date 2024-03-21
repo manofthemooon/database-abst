8f7cc3921f78366cf00f21c46200096f16faa105
Andrey^ <manofthemoonwork@gmail.com>
2024-03-21 12:47:40 +0300

    second commit

---
+++ src/index.js
@@ -0,0 +1,51 @@
+// db.js
+
+const sqlite3 = require('sqlite3').verbose();
+
+class Database {
+  constructor(dbPath) {
+    this.db = new sqlite3.Database(dbPath);
+  }
+
+  run(query, params = []) {
+    return new Promise((resolve, reject) => {
+      this.db.run(query, params, function(err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve({ lastID: this.lastID, changes: this.changes });
+        }
+      });
+    });
+  }
+
+  get(query, params = []) {
+    return new Promise((resolve, reject) => {
+      this.db.get(query, params, (err, row) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(row);
+        }
+      });
+    });
+  }
+
+  all(query, params = []) {
+    return new Promise((resolve, reject) => {
+      this.db.all(query, params, (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  }
+
+  close() {
+    this.db.close();
+  }
+}
+
+module.exports = Database;
